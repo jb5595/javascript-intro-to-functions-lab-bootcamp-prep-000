afb88e78e0475fc457e57af01dc2b1d209b1a358
Jacob Berman <jacobberman1995@gmail.com>
2018-03-23 12:57:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,11 +15,10 @@ function logShout(string){
 }
 function sayHiToGrandma(string){
   if (string === string.toLowerCase){
-    response = "I can't hear you!"
-    return response
+    return "I can't hear you! "
   }
   if (string === string.toUpperCase){
-    return 'YES INDEED!'
+    return 'YES INDEED! '
   }
   if (string === "I love you, Grandma."){
     return "I love you, too."
